52c8efe5e2b37aeded623996acb0ae8fdba432a2
annam <s27684@pjwstk.edu.pl>
2025-11-16 20:20:10 +0100

    Updated models

--- backend/Models/TripPhoto.cs
+++ backend/Models/TripPhoto.cs
@@ -3,28 +3,35 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace backend.Models;
 
+[Table("trip_photo")]
 public class TripPhoto
 {
+    [Column("id_trip_photo")]
     [Key]
     public int IdTripPhoto { get; set; }
     
+    [Column("id_trip")]
     [Required]
     [ForeignKey(nameof(Trip))]
     public int IdTrip { get; set; }
     public Trip Trip { get; set; } = null!;
 
     //user that added photo
+    [Column("uploaded_by")]
     [Required]
     [ForeignKey(nameof(UploadedByUser))]
     public int UploadedBy { get; set; }
     public User UploadedByUser { get; set; } = null!;
 
+    [Column("photo_path")]
     [Required]
     [MaxLength(255)]
     public string PhotoPath { get; set; } = null!;
 
+    [Column("description")]
     [MaxLength(500)]
     public string? Description { get; set; }
 
+    [Column("uploaded_at")]
     public DateTime UploadedAt { get; set; } = DateTime.UtcNow;
 }
--- backend/Models/UserChat.cs
+++ backend/Models/UserChat.cs
@@ -3,13 +3,16 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace backend.Models;
 
+[Table("user_chat")]
 public class UserChat
 {
+    [Column("id_chat")]
     [Required]
     [ForeignKey(nameof(Chat))]
     public int IdChat { get; set; }
     public Chat Chat { get; set; } = null!;
     
+    [Column("id_user")]
     [Required]
     [ForeignKey(nameof(User))]
     public int IdUser { get; set; }
