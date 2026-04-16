03669b523a949dfb2007f8684eaa2ea9248881bf
annam <s27684@pjwstk.edu.pl>
2026-04-16 22:21:40 +0200

    Removed img icon in profile header

--- frontend/src/views/Profile/ProfileHeader.js
+++ frontend/src/views/Profile/ProfileHeader.js
@@ -97,32 +97,6 @@ export default function ProfileHeader({
                                     </svg>
                                 </IconButton>
                             )}
-                            <IconButton title="Zdjęcia" onClick={onGalleryClick}>
-                                <svg
-                                    viewBox="0 0 24 24"
-                                    width="18"
-                                    height="18"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        d="M4 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V7Z"
-                                        stroke="currentColor"
-                                        strokeWidth="2"
-                                    />
-                                    <path
-                                        d="M8.5 10.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z"
-                                        fill="currentColor"
-                                    />
-                                    <path
-                                        d="M21 16l-5-5-6 6"
-                                        stroke="currentColor"
-                                        strokeWidth="2"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                    />
-                                </svg>
-                            </IconButton>
                         </div>
 
                         <div className="ph-username">@{username}</div>
